4ba0e2ec11f13a75353e1cebe0332cc5e7902921
Mihai Filat <mihai.filat@yahoo.com>
2024-04-21 17:16:25 +0200

    rules for keeping/ignoring

--- main.py
+++ main.py
@@ -31,19 +31,44 @@ class File:
         self.extension = file.split('.')[-1]
 
 
-def match_rule(rules, file):
-    for rule in rules:
+def get_size(directory):
+    total_size = 0
+    for file in os.scandir(directory):
+        total_size += os.stat(file).st_size
+
+    return total_size
+
+
+def match_rule(rules: dict, full_path: str):
+    """
+    :param rules: dict containing rules for keeping/ignoring files
+    :param full_path: full path of file or directory
+    :return: False if file should be ignored, True if file should be kept
+    If file does not match any rule, it returns None
+    """
+
+    if rules['ignore_empty']:
+        if is_empty(full_path):
+            return False
+
+    file = os.path.basename(full_path)
+
+    for rule in rules['ignore']:
+        if fnmatch(file, rule):
+            return False
+
+    for rule in rules['keep']:
         if fnmatch(file, rule):
             return True
 
-    return False
+    return None
 
 
 def is_empty(path):
-    size = sum(file.stat().st_size for file in Path(path).rglob('*'))
-    if size == 0:
-        return True
-    return False
+    if os.path.isfile(path):
+        return os.path.getsize(path) == 0
+    elif os.path.isdir(path):
+        return sum(file.stat().st_size for file in Path(path).rglob('*')) == 0
 
 
 def get_highest_version(files):
@@ -88,27 +113,33 @@ def get_highest_version(files):
 
 
 
-def check_project(path: str, dir_rules: list, file_rules: list, ignore_empty_dirs: bool = True):
+def check_project(path: str, dir_rules: dict, file_rules: dict):
     ignore = []
     keep = []
 
     for root, dirs, files in os.walk(path):
-        # Check if directories match rules and add them to ignore list
-        dirs_to_remove = [dr for dr in dirs if match_rule(dir_rules, dr)]
-        for dr in dirs_to_remove:
+        # If dirs match ignore rule, they are ignored
+        # If dirs match keep rule, all the contents are kept
+        # Iterate over copy of dirs so we can remove items from original list
+        for dr in dirs[:]:
             full_path = os.path.join(root, dr)
-            ignore.append(full_path)
+            match = match_rule(dir_rules, full_path)
 
-            dirs.remove(dr)
-            print(f"{Fore.MAGENTA}{full_path}{Fore.RESET}")
+            if match is None:
+                continue
 
-        # ignore empty directories
-        if ignore_empty_dirs and is_empty(root):
-            ignore.append(root)
-            print(f"{Fore.LIGHTBLACK_EX}{root}{Fore.RESET}")
+            if match is False:
+                ignore.append(full_path)
+                dirs.remove(dr)
+                print(f"{Fore.LIGHTBLACK_EX}{full_path}{Fore.RESET}")
+
+            if match is True:
+                keep.append(full_path)
+                dirs.remove(dr)
+                print(f"{Fore.MAGENTA}{full_path}{Fore.RESET}")
 
         # Print root directory if it contains files
-        elif len(files) != 0:
+        if len(files) != 0:
             print(root)
 
         # Get files that should be kept
@@ -117,42 +148,43 @@ def check_project(path: str, dir_rules: list, file_rules: list, ignore_empty_dir
         for file in files:
             print('― ', end='')
             full_path = os.path.join(root, file)
+            match = match_rule(file_rules, full_path)
 
-            if match_rule(file_rules, file):
-                ignore.append(full_path)
-                print(f"{Fore.LIGHTBLACK_EX}{file}{Fore.RESET}")
-                continue
+            if match is None:
+                # Keep files according to versioned_files and single_files
+                if file in versioned_files or file in single_files:
+                    keep.append(full_path)
+                    color = Fore.GREEN if file in versioned_files else Fore.YELLOW
+                    print(f"{color}{file}{Fore.RESET}")
+                else:
+                    ignore.append(full_path)
+                    print(file)
 
-            # Empty files are ignored
-            if os.path.getsize(full_path) == 0:
-                ignore.append(full_path)
-                print(f"{Fore.LIGHTBLACK_EX}{file}{Fore.RESET}")
-                continue
-
-            # Keep files according to versioned_files and single_files
-            if file in versioned_files or file in single_files:
+            if match is True:
                 keep.append(full_path)
-                color = Fore.GREEN if file in versioned_files else Fore.YELLOW
-                print(f"{color}{file}{Fore.RESET}")
-            else:
+                print(f"{Fore.MAGENTA}{file}{Fore.RESET}")
+
+            if match is False:
                 ignore.append(full_path)
-                print(file)
+                print(f"{Fore.LIGHTBLACK_EX}{file}{Fore.RESET}")
+
 
     return ignore, keep
 
 
-def archive_project(path: str, org_path: str, to_keep: list):
+def archive_project(dest_path: str, org_path: str, to_keep: list):
     for file in to_keep:
-        # Get relative paths
-        src_dir = os.path.dirname(file)
-        src_file = os.path.basename(file)
-
         rel_path = os.path.relpath(file, org_path)
-        dest_dir = os.path.dirname(os.path.join(path, rel_path))
+        dest = os.path.join(dest_path, rel_path)
 
-        print('Copying: ', os.path.join(dest_dir, src_file))
-        copy_cmd = ['robocopy', src_dir, dest_dir, src_file, '/J', '/NJS', '/NJH', '/NDL']
+
+        print('Copying: ', os.path.join(dest, file))
+        if os.path.isdir(file):
+            copy_cmd = ['robocopy', file, dest, '/J', '/NJS', '/NJH', '/NDL', '/S']
+        else:
+            copy_cmd = ['robocopy', os.path.dirname(file), os.path.dirname(dest), os.path.basename(file), '/J', '/NJS', '/NJH', '/NDL']
         subprocess.run(copy_cmd, shell=True)
+
 
 
 
@@ -183,25 +215,37 @@ def test(project_path, to_ignore, to_keep):
 
 if __name__ == '__main__':
     dir_rules = {
+        'ignore_empty': True,
         'ignore': [
             '.*',
             '_archive',
-            '*dailies'
+            'tex'
+        ],
+        'keep': [
+            '*dailies',
+        ]
+    }
+
+    file_rules = {
+        'ignore_empty': True,
+        'ignore': [
+            '*.tx',
+            'Thumbs.db'
         ],
         'keep': [
         ]
     }
 
-    file_rules = [
-        '*.tx',
-        'Thumbs.db'
-    ]
-
-    to_ignore, to_keep = check_project(r'D:\CreatureProject', dir_rules['ignore'], file_rules, ignore_empty_dirs=True)
+    to_ignore, to_keep = check_project(r'D:\CreatureProject', dir_rules, file_rules)
 
     if test(r'D:\CreatureProject', to_ignore, to_keep):
         print('All tests passed')
-        # archive_project(r'D:\CreatureProject_archive', r'D:\CreatureProject', to_keep)
+        archive_project(r'D:\CreatureProject_archive', r'D:\CreatureProject', to_keep)
+
+        archive_size = sum(file.stat().st_size for file in Path(r'D:\CreatureProject_archive').rglob('*'))
+        print(archive_size)
+        test(r'D:\CreatureProject', to_ignore, to_keep)
+
 
 
 
